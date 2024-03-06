47f2f662b5a5def8b5132322a622cc7c52ca4f84
Kevin Do <do.kevin.vo@gmail.com>
2024-03-05 20:21:22 -0800

    update: clean up javascript import

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,17 @@
 /// <reference types="astro/client" />
+
+declare function customSlider(
+  minWidth: number,
+  maxWidth: number,
+  targetClassName: string | null,
+  duration: number
+): void;
+
+interface Window {
+  customSlider(
+    minWidth: number,
+    maxWidth: number,
+    targetClassName: string | null,
+    duration: number
+  ): void;
+}
