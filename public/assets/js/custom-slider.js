da32300c984b81886a49a4d6dc69437acf2dc0dd
Kevin Do <do.kevin.vo@gmail.com>
2024-04-08 01:27:45 -0700

    🚧: adds lozad to lazyload image with blur

--- public/assets/js/custom-slider.js
+++ public/assets/js/custom-slider.js
@@ -1,4 +1,3 @@
-// v16 - add trim()
 function customSlider({
   minWidth = 768,
   maxWidth = 767,
@@ -43,23 +42,23 @@ function customSlider({
     linkElem.as = "image";
 
     const doesDesktopLinkExist = document.querySelector(
-      'link[href="' + slider[index].children[0].srcset.trim() + '"]',
+      'link[href="' + slider[index].children[0].srcset + '"]',
     );
 
     const doesMobileLinkExist = document.querySelector(
-      'link[href="' + slider[index].lastElementChild.src.trim() + '"]',
+      'link[href="' + slider[index].lastElementChild.src + '"]',
     );
 
     if (!doesDesktopLinkExist) {
       var linkDesktopElem = linkElem.cloneNode(false);
-      linkDesktopElem.href = slider[index].children[0].srcset.trim();
+      linkDesktopElem.href = slider[index].children[0].srcset;
       linkDesktopElem.media = "(min-width: 768px)";
       document.head.appendChild(linkDesktopElem);
     }
 
     if (!doesMobileLinkExist) {
       var linkMobileElem = linkElem.cloneNode(false);
-      linkMobileElem.href = slider[index].lastElementChild.src.trim();
+      linkMobileElem.href = slider[index].lastElementChild.src;
       linkMobileElem.media = "(max-width: 767px)";
       document.head.appendChild(linkMobileElem);
     }
